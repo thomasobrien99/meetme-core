3ebc0868d1a75dd6324f075da8e6cafd8edd2c48
Thomas O'Brien <thomas.obrien@apprennet.com>
2017-06-10 13:33:46 -0700

    fix typo

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('*', (req, res) => {
   yelp.searchBusiness({ term: 'ice cream' })
   .then(results => {
-    res.send(results));
+    res.send(results);
   });
 });
 
