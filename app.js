d42208ff6c699a60dafe6c63471d4d236309ff65
Thomas O'Brien <thomas.obrien@apprennet.com>
2017-06-10 13:28:24 -0700

    remove index route

--- app.js
+++ app.js
@@ -22,9 +22,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
-
 app.use('/api/businesses/search', (req, res) => {
   yelp.searchBusiness({ term: 'ice cream' })
   .then(results => {
