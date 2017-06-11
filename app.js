b5088033f78f05e5dda4022ab2d3fb013c82af08
Thomas O'Brien <thomas.obrien@apprennet.com>
2017-06-11 11:33:35 -0700

    enable cors

--- app.js
+++ app.js
@@ -19,9 +19,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/api/businesses/search', (req, res) => {
   yelp.searchBusiness({
-    latitude: Number(req.query.lat), 
+    latitude: Number(req.query.lat),
     longitude: Number(req.query.lng),
     term: 'ice cream'
   })
